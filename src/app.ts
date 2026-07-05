9d72be42d0933e6cde73b2a1b3f20386b556f816
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-07-05 18:36:26 +0600

    added project initial setup also, express and database connection

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import cookieParser from "cookie-parser";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import config from "./config";
+
+const app: Application = express();
+
+// Middlewares
+app.use(cors({
+    origin: config.appUrl,
+    credentials: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// All Routes
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+
+export default app;
---
+++ src/config/index.ts
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+    path: path.join(process.cwd(), ".env")
+});
+
+const config = {
+    databaseUrl: process.env.DATABASE_URL,
+    port: process.env.PORT,
+    appUrl: process.env.APP_URL,
+
+    security: {
+        bcryptSaltRounds: process.env.BCRYPT_SALT_ROUNDS,
+    },
+
+    jwt: {
+        accessSecret: process.env.JWT_ACCESS_SECRET!,
+        refreshSecret: process.env.JWT_REFRESH_SECRET!,
+        accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN!,
+        refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN!,
+    },
+
+    stripe: {
+        productId: process.env.STRIPE_PRODUCT_PRICE_ID!,
+        secretKey: process.env.STRIPE_SECRET_KEY!,
+        webhookSecret: process.env.STRIPE_WEBHOOK_SECRET!
+    }
+};
+
+export default config;
